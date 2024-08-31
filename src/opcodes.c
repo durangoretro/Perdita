806d77aa49e84db75acf0d3eb47b10b6e084bdc2
Carlos J. Santisteban <zuiko21@gmail.com>
2024-08-31 12:02:11 +0200

    attempt to fix BCD addition

--- src/opcodes.c
+++ src/opcodes.c
@@ -55,7 +55,7 @@ void adc(byte d) {
 	big += (p & 1);			// add with Carry (A was computer just after this)
 
 	if (p & 0b00001000) {						// Decimal mode!
-		high = (old & $F0)+(d & $F0)			// compute carry-less LSN
+		high = (old & $F0)+(d & $F0);			// compute carry-less LSN
 		if (((big & 0x0F) > 9)||(high != (big & $F0))) {		// LSN overflow? was 'a' instead of 'big'
 			big += 6;											// get into next decade
 		}
